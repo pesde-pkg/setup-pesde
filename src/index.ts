23a33bfc7c7b90f5f6ae73ba772b7b8ec3501ffb
Erica Marigold <hi@devcomp.xyz>
2025-10-29 05:15:42 +0000

    feat: only monitor heap peak usage for debugging

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
+import { isDebug } from "./logging.js";
 import { ToolManager } from "./tool.js";
 import { humanReadableSize } from "./util.js";
+import logger from "./logging.js";
 
-let peakHeap = 0;
-const monitorInterval = setInterval(() => {
-  const { heapUsed } = process.memoryUsage();
-  if (heapUsed > peakHeap) peakHeap = heapUsed;
-}, 500); // check every 500ms
+// monitor peak heap usage for debugging
+if (isDebug()) {
+  var peakHeap = 0;
+  var monitorInterval = setInterval(() => {
+    const { heapUsed } = process.memoryUsage();
+    if (heapUsed > peakHeap) peakHeap = heapUsed;
+  }, 500); // check every 500ms
+
+}
 
 await new ToolManager("lune-org", "lune")
-	.version((v) => v.match(/^v?0\.10(?:\.\d+)?$/g) != null)
-	.install()
-	.finally(() => clearInterval(monitorInterval));
-
-console.log("Peak heap usage: ", humanReadableSize(peakHeap))
+  .version((v) => v.match(/^v?0\.10(?:\.\d+)?$/g) != null)
+  .install()
+  .finally(() => {
+    if (isDebug()) {
+      clearInterval(monitorInterval);
+      logger.debug("Peak heap usage: %s", humanReadableSize(peakHeap))
+    }
+  });
 
 
 // todo: structured errors
